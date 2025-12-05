a1eb1992df97c5e33fa8c28a2fc3296052614064
Chengyd <niuchengy@163.com>
2025-12-05 22:11:02 +0800

    four test

--- src/main/java/com/ncy/TestMain.java
+++ src/main/java/com/ncy/TestMain.java
@@ -7,5 +7,6 @@ public class TestMain {
         System.out.println("Hello git2!");
         System.out.println("Hello git3!");
         System.out.println("Hello git4!");
+        System.out.println("Hello hot-fix!");
     }
 }
