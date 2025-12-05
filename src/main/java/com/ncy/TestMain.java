6c0ffa4ef580e92ce8950b1a37188954130eabcb
Chengyd <niuchengy@163.com>
2025-12-05 22:01:19 +0800

    second test

--- src/main/java/com/ncy/TestMain.java
+++ src/main/java/com/ncy/TestMain.java
@@ -4,5 +4,6 @@ public class TestMain {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello git!");
+        System.out.println("Hello git2!");
     }
 }
