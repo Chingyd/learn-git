6b9c1f59422f258a44abb6d136f63193274b15ba
Chengyd <niuchengy@163.com>
2025-12-05 22:15:37 +0800

    master commit 2

--- src/main/java/com/ncy/TestMain.java
+++ src/main/java/com/ncy/TestMain.java
@@ -8,5 +8,6 @@ public class TestMain {
         System.out.println("Hello git3!");
         System.out.println("Hello git4!");
         System.out.println("Hello hot-fix!");
+        System.out.println("Hello master!");
     }
 }
